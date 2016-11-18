7f182aa1bfebec99bf7c2d7cbddb618c9f9ff048
Dogdriip <dogdriip@gmail.com>
2016-11-18 07:42:15 +0000

    아몰랑개귀찮아

--- core.rb
+++ core.rb
@@ -1,15 +1,19 @@
 require 'rubygems'
 require 'sinatra/base'
 require 'sinatra/assetpack'
+require 'sinatra-websocket'
 
 class CoditFestival2016 < Sinatra::Base
+  set :server, 'thin'
+  set :sockets, []
+  set :bind, '0.0.0.0'
+
   set :root, File.dirname(__FILE__)
   register Sinatra::AssetPack
   assets do
     serve '/js', from: 'app/js'
     serve '/css', from: 'app/css'
     serve '/images', from: 'app/images'
-
     js :application, [ '/js/jquery.min.js', '/js/underscore-min.js', '/js/backbone-min.js', '/js/*.js' ]
     css :application, [ '/css/*.css' ]
     js_compression :jsmin
@@ -22,14 +26,63 @@ class CoditFestival2016 < Sinatra::Base
     end
   end
 
-  # temp
-  set :bind, '0.0.0.0'
+  # authentication
+  helpers do
+    def protected!
+      return if authorized?
+      headers['WWW-Authenticate'] = 'Basic realm="Restricted Area"'
+      halt 401, "Not authorized\n"
+    end
 
-  get '/' do # 인덱스
-    erb :index, :layout => !request.pjax?
+    def authorized?
+      @auth ||=  Rack::Auth::Basic::Request.new(request.env)
+      @auth.provided? and @auth.basic? and @auth.credentials and @auth.credentials == ['admin', 'jungchulisajae']
+    end
+  end
+
+  get '/admin' do
+    protected!
+    if !request.websocket?
+      erb :admin, :layout => !request.pjax?
+    else
+      request.websocket do |ws|
+        ws.onopen do
+          ws.send("Hello World!")
+          settings.sockets << ws
+        end
+        ws.onmessage do |msg|
+          EM.next_tick { settings.sockets.each{|s| s.send(msg) } }
+        end
+        ws.onclose do
+          warn("websocket closed")
+          settings.sockets.delete(ws)
+        end
+      end
+    end
+  end
+
+
+  get '/' do
+    if !request.websocket?
+      erb :index, :layout => !request.pjax?
+    else
+      request.websocket do |ws|
+        ws.onopen do
+          ws.send("Hello World!")
+          settings.sockets << ws
+        end
+        ws.onmessage do |msg|
+          EM.next_tick { settings.sockets.each{|s| s.send(msg) } }
+        end
+        ws.onclose do
+          warn("websocket closed")
+          settings.sockets.delete(ws)
+        end
+      end
+    end
   end
   
-  get '/booth' do # 부스 배치도 / 목록
+  get '/booth' do
     erb :booth, :layout => !request.pjax?
   end
 
@@ -40,9 +93,7 @@ class CoditFestival2016 < Sinatra::Base
     erb :about, :layout => !request.pjax?
   end
 
-
-
-
+  
 
   get '/qr/test' do
     erb :'qr/test', :layout => !request.pjax?
